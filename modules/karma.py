3860d92b0b4ee4a7a119674561cd939d9fdbf124
Ori Rawlings <ori.rawlings@orbitz.com>
2011-04-07 09:59:43 -0500

    Added sqlite3 persistence to karma data. Added additional ways of specifiying karma rewards or punishment.

--- modules/karma.py
+++ modules/karma.py
@@ -4,9 +4,32 @@ karma.py - Phenny karma module
 By Ori Rawlings
 """
 
+import sqlite3
+import os
 from itertools import imap
+from functools import partial
+from operator import add
 
-points = {}
+
+ops = { '++':partial(add, 1), '+1':partial(add, 1), '--':partial(add, -1), '-1':partial(add, -1) }
+
+class KarmaDAO:
+    def __init__(self, path):
+        self.db_path = path
+
+    def karmas(self, conn):
+        return conn.execute('select name,score from karma order by score desc')
+
+    def update_karma(self, conn, user, f, default=0):
+        user = str(user)
+        score = conn.execute('select score from karma where name = ?', (user,)).fetchone()
+        if score:
+            score = f(score[0] or default)
+            conn.execute('update karma set score=? where name=?', (score,user))
+        else:
+            score = f(default)
+            conn.execute('insert into karma (name,score) values (?,?)', (user,score))
+        conn.commit()
 
 def karma_point(phenny, input):
     """
@@ -22,31 +45,33 @@ def karma_point(phenny, input):
     """
     user = input.group(1)
     op = input.group(2)
-    if op == '++':
-        if input.nick == user:
-            phenny.say('Silly ' + input.nick + ", you can't award yourself karma...")
+    if input.nick == user:
+        if '+' in op:
+            phenny.reply("Silly, you can't award yourself karma...")
             return
-        f = lambda x: x+1
-    elif op == '--':
-        if input.nick == user:
-            phenny.say('Wow ' + input.nick + ", you must have really been bad to take karma from yourself...")
-        f = lambda x: x-1
-    else:
-        f = lambda x: x
-    try:
-        points[user] = f(points[user])
-    except KeyError:
-        points[user] = f(0)
-karma_point.rule = '^(\w+)(\+{2}|-{2})$'
+        elif '-' in op:
+            phenny.reply("Wow, you must have really been bad to take karma from yourself...")
+    f = ops.get(op, lambda x: x)
+    conn = sqlite3.connect(phenny.karma_dao.db_path)
+    phenny.karma_dao.update_karma(conn, user, f, 0)
+karma_point.rule = '^(\w+)(?:: )?(\+{2}|\+1|-{2}|-1)\s*(.*)$'
 
 def karma(phenny, input):
     """
     .karma - prints all stored karma scores for various nicks to the channel
     """
-    for entry in points.items():
+    conn = sqlite3.connect(phenny.karma_dao.db_path)
+    for entry in phenny.karma_dao.karmas(conn):
         phenny.say('\t'.join(imap(str,entry)))
+    conn.close()
 karma.commands = ['karma']
 karma.priority = 'medium'
+
+def path(self):
+    return os.path.join(os.path.expanduser('~/.phenny'),'karma','karma.db')
+
+def setup(phenny):
+    phenny.karma_dao = KarmaDAO(path(phenny))
 
 if __name__ == '__main__': 
    print __doc__.strip()
